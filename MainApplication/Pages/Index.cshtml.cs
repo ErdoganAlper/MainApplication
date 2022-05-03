354ad004401fec9dd4cdd6a95e46f3b0d5dd8b90
Erdogan Alper <alper.e@unic.ac.cy>
2022-05-03 16:08:05 +0300

    Add project files.

---
+++ MainApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public int num1 { get; set; }
+
+        [BindProperty]
+        public int num2 { get; set; }
+        public int Num3 { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
